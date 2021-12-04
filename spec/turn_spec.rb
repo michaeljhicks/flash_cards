a698be479d2881d6462bf0505b260bd072171d5a
Michael Hicks <hicks.michael04@gmail.com>
2021-12-04 13:15:48 -0700

    Add #is corect answer test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,9 +12,15 @@ RSpec.describe Turn do
   end
 
   it "has a guess" do
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     expect(turn.guess).to eq("Juneau")
+  end
+
+  it "is correct answer" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to eq(true)
   end
 
 end
