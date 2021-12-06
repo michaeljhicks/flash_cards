cf6aa541968d5c5b3465b43703912c899df5edf1
Michael Hicks <hicks.michael04@gmail.com>
2021-12-06 07:12:27 -0700

    Run out of time and sleep

---
+++ flashcard_runner.rb
@@ -0,0 +1,13 @@
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+require './lib/round'
+require 'pry'
+require 'rspec'
+# Create some Cards
+# Put those cards into a Deck
+# Create a new Roiund using the Deck you created
+# Start the round using a new method called <start>
+
+
+puts "Welcome! You're playing with 4 cards.\n-------------------------------------------------\n"
